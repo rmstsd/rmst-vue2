9bef4e86aabcbc227a167a202be01ad29d5d36e5
春雷-李春雷 <chunlei.lcl@naturobot.com>
2023-07-09 20:03:45 +0800

    s

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <VirtualList
+      class="list"
+      :data-key="'id'"
+      :data-sources="items"
+      :data-component="Item"
+      :estimate-size="30"
+      item-class="list-item-fixed"
+      :keeps="60"
+    >
+      <!-- <div slot="header" class="header-m">header</div>
+      <div slot="footer" class="loading-spinner-m">Loading ...</div> -->
+    </VirtualList>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import VirtualList from 'vue-virtual-scroll-list'
+
+  import Item from './Item.vue'
+
+  const TOTAL_COUNT = 10000
+
+  const DataItems = []
+  let count = TOTAL_COUNT
+  while (count--) {
+    const index = TOTAL_COUNT - count
+    DataItems.push({
+      index,
+      name: index + '-name',
+      id: index
+    })
+  }
+
+  const items = ref(DataItems)
+</script>
+
+<style>
+  .header-m {
+    height: 100px;
+    background-color: pink;
+  }
+  .loading-spinner-m {
+    height: 50px;
+  }
+  .list {
+    width: 100%;
+    height: 500px;
+    border: 2px solid;
+    border-radius: 3px;
+    overflow-y: auto;
+    border-color: dimgray;
+  }
+
+  .list .list-item-fixed {
+    display: flex;
+    align-items: center;
+    padding: 0 1em;
+    height: 30px;
+    border-bottom: 1px solid;
+    border-color: lightgray;
+  }
+</style>
---
+++ src/Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="item-inner">
+    <span># {{ source.index }}</span>
+    <span>{{ source.name }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'fix-size-item',
+
+    props: {
+      source: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    },
+    created() {
+      let t = Date.now()
+
+      while (Date.now() - t < 10) {}
+    }
+  }
+</script>
+
+<style scoped>
+  .item-inner span:first-child {
+    margin-right: 1em;
+  }
+</style>
